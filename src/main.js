ec905a912f14d847ac1573bf38ce31a7ac6a006d
Natalia Quezada <66387884+nsquezadam@users.noreply.github.com>
2020-07-03 15:41:34 -0400

    Se estructura HTML y se agrega html de data

--- src/main.js
+++ src/main.js
@@ -1,6 +1,27 @@
-import { example } from './data.js';
+import { } from './data.js';
 // import data from './data/atletas/atletas.js';
 // import data from './data/lol/lol.js';
 import data from './data/pokemon/pokemon.js';
 
-console.log(example, data);
+/* console.log('a');
+console.table(data.pokemon);
+console.log('b');
+console.log(data.pokemon.length);
+*/
+
+
+let newData = data.pokemon;
+
+
+// me retorna toda la data
+
+// let newData = data.pokemon.filter(item => {
+//  return item.num < 100
+// })
+// console.log ("data.pokemon =>",data.pokemon);
+// console.log ("newData =>", newData);
+
+/* let newData = data.pokemon.filter(( item, index, array) => {
+   document.write(item.name, index )
+});
+*/
