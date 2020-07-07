c6f854d25654e2d65dab1b5942e9736bc44547d0
Natalia Quezada <66387884+nsquezadam@users.noreply.github.com>
2020-07-06 20:58:18 -0400

    actualizacion

--- src/main.js
+++ src/main.js
@@ -3,13 +3,11 @@ import {} from './data.js';
 // import data from './data/lol/lol.js';
 import data from './data/pokemon/pokemon.js';
 
-/* console.log('a');
-console.table(data.pokemon);
-console.log('b');
-console.log(data.pokemon.length);
-*/
-// eslint-disable-next-line no-console
-console.table(data.pokemon);
+const pokemonNames = [];
 
-// et newData = data.pokemon;
-cons new
+
+for (let i = 0; i < data.pokemon.length; i++) {
+  pokemonNames.push(data.pokemon[i].name);
+}
+// eslint-disable-next-line no-console
+console.table(pokemonNames);
